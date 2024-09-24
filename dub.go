b00605546dcb27dcc27586d2939c9de9a52080d4
regression-test-executor <regression-test-executor@speakeasy.com>
2024-09-24 17:35:12 +0000

    openapi-generation@71c220ff0f346c7ee88c1fe8f0c72b85f604909f

--- dub.go
+++ dub.go
@@ -158,8 +158,8 @@ func New(opts ...SDKOption) *Dub {
 			Language:          "go",
 			OpenAPIDocVersion: "0.0.1",
 			SDKVersion:        "0.9.4",
-			GenVersion:        "2.422.6",
-			UserAgent:         "speakeasy-sdk/go 0.9.4 2.422.6 0.0.1 github.com/dubinc/dub-go",
+			GenVersion:        "2.422.16",
+			UserAgent:         "speakeasy-sdk/go 0.9.4 2.422.16 0.0.1 github.com/dubinc/dub-go",
 			Hooks:             hooks.New(),
 		},
 	}
